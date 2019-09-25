e76c9ce15741a1bff36a3c472b13a7b21f8c6d30
MSW-yar <apnashery96@yahoo.com>
2019-09-25 16:05:02 +0500

    added showbits

--- C plans/samplelanguage.c
+++ C plans/samplelanguage.c
@@ -390,3 +390,30 @@
 //     b = b ^ 12 ;
 //     printf ( "\n%d", b ) ; /* this will print 50 */
 // }
+
+// void showbits(unsigned int x)
+// {
+//     int i;
+
+//     for(i=(sizeof(int)*8)-1; i>=0; i--)
+//     {
+//         (x&(1u<<i)) ? putchar('1'): putchar('0');
+//     }
+//     printf("\n");
+// }
+// int main()
+// {
+//     int j = 5225, m, n;
+
+//     printf("%d in binary \t\t ", j);
+//     showbits(j);
+
+//     // the loop for right shift operation
+//     for ( m = 0; m <= 5; m++ )
+//     {
+//         n = j >> m;
+//         printf("%d right shift %d gives ", j, m);
+//         showbits(n);
+//     }
+//     return 0;
+// }
